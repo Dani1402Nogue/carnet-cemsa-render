8db89891c8b9348f7602bcd1162d3171c9f1b1af
Dani1402Nogue <daniel.noguerol@hotmail.com.ar>
2025-05-08 23:29:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -41,3 +41,7 @@ def descargar():
 
     driver.quit()
     return result
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render define esta variable automáticamente
+    app.run(host="0.0.0.0", port=port)
